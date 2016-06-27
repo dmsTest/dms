f79bcdacc9ffb7a7d9c506844d23203a880a4f38
tanligao <1002064524@qq.com>
2016-06-27 15:20:05 +0800

    add ClientException.h and ClientException.cpp

---
+++ client/ClientException.cpp
@@ -0,0 +1,85 @@
+nclude "ClientException.h"
+
+ClientException::ClientException()
+{
+	    //ctor
+	    //
+}
+
+ClientException::ClientException(const std::string &str) : m_msg(str)
+{
+	    //ctor
+	    //
+}
+
+const char*  ClientException::what()
+{
+	    return m_msg.c_str();
+
+}
+ClientException::~ClientException()
+{
+	    //dtor
+	    //
+}
+
+BackupException::BackupException() : ClientException()
+{
+	
+
+}
+
+BackupException::BackupException(const std::string &str) : ClientException(str)
+{
+	
+
+}
+
+SocketException::SocketException() : ClientException()
+{
+	
+
+}
+
+SocketException::SocketException(const std::string &str) : ClientException(str)
+{
+	
+
+}
+
+ReadException::ReadException()
+{
+	
+
+}
+
+ReadException::ReadException(const std::string &str) : ClientException(str)
+{
+	
+
+}
+
+SendException::SendException() : ClientException()
+{
+	
+
+}
+
+SendException::SendException(const std::string &str) : ClientException(str)
+{
+	
+
+}
+
+SaveException::SaveException() : ClientException()
+{
+	
+
+}
+
+SaveException::SaveException(const std::string &str) : ClientException(str)
+{
+	
+
+}
+
---
+++ client/ClientException.h
@@ -0,0 +1,61 @@
+fndef CLIENTEXCEPTION_H
+#define CLIENTEXCEPTION_H
+
+#include <exception>
+#include <string>
+#include <iostream>
+
+class ClientException : public std::exception
+{
+	public:
+		    ClientException();
+		        ClientException(const std::string &str);
+			    virtual const char* what() ;
+			        virtual ~ClientException();
+	private:
+				    std::string m_msg;
+
+};
+
+class BackupException : public ClientException
+{
+	public:
+		    BackupException();
+		        BackupException(const std::string &str);
+
+};
+
+class SocketException : public ClientException
+{
+	public:
+		    SocketException();
+		        SocketException(const std::string &str);
+
+};
+
+class ReadException : public ClientException
+{
+	public:
+		    ReadException();
+		        ReadException(const std::string &str);
+
+};
+
+class SendException : public ClientException
+{
+	public:
+		    SendException();
+		        SendException(const std::string &str);
+
+};
+
+class SaveException : public ClientException
+{
+	public:
+		    SaveException();
+		        SaveException(const std::string &str);
+
+};
+
+#endif // CLIENTEXCEPTION_H
+
