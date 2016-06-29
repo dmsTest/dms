c7adb3f83ba73fef29e17a49886820268ed3d0e1
tanligao <1002064524@qq.com>
2016-06-29 10:24:45 +0800

    update main.cpp

--- client/main.cpp
+++ client/main.cpp
@@ -9,19 +9,38 @@
 using namespace std;
 
 std::string m_ip = "127.0.0.1";
-int m_port = 8888;
+short m_port = 8888;
 
 int main(int argc, char** argv)
 {
-	std::string logFile = "wtmpx";
-	std::string loginFile = "";
-	std::string failFile = "";
-	LogReader reader(logFile,loginFile);
-	SocketSender socketSender(failFile,m_port,m_ip);
-	Client client(reader,socketSender);
-	
-	client.dataMine();
-
+	if(argc < 2)
+	{
+		try{
+			LogReader reader("./wtmpx","./logins.dat");
+			ConsoleSender consoleSender;
+			//SocketSender socketSender("./fail.dat",m_port,m_ip);
+			//Client client(reader,socketSender);
+			Client client(reader,consoleSender);
+			client.dataMine();
+		} catch(ClientException& ex)
+		{
+			cout << ex.what() << endl;
+			return -1;
+		}
+	}
+	else
+	{
+		if(!strcmp(argv[1],"-g"))
+		{
+			cout << "start the gui..." << endl;
+			return -1;
+		}
+		else
+		{
+			cout << "format is error,eg: ./main -g" << endl;
+			return 0;
+		}
+	}
 	return 0;
 }
 
