08f372793e3754dfc329677b0ffd65b381fda2c8
929162778 <m18829350162@163.com>
2016-06-30 16:58:06 +0800

    alter LogReader.h define

--- client/LogReader.h
+++ client/LogReader.h
@@ -6,28 +6,29 @@
 #include <list>
 #include "data.h"
 #include "ClientException.h"
-
+using namespace std;
 
 class LogReader
 {
-    public:
-        LogReader(const std::string &logFile, const std::string &loginFile);
-         std::list<MLogRec>&  readLog() ;
-        virtual ~LogReader();
+public:
+    LogReader(const string &logFile, const string &loginFile);    //¹¹Ôìº¯Êý µÇÂ¼¼ÇÂ¼
+    list<MLogRec>& readLog();
+    virtual ~LogReader();
 
-    private:
-        std::string m_logFile;
-        std::string m_loginsFile;
-        std::string m_backupFile;
-        std::list<LogRec> m_logins;
-        std::list<LogRec> m_logouts;
-        std::list<MLogRec> m_logs;
+private:
+    string m_logFile;         //原始日志记录文件
+    string m_loginsFile;      //未匹配的日志记录文件
+    string m_backupFile;      //备份的日志记录文件
+    list<LogRec> m_logins;    //登入记录表
+    list<LogRec> m_logouts;   //登出记录表
+    list<MLogRec> m_logs;     //匹配成功的日志记录表
 
-        void backup() throw(BackupException);
-        void readLoginsFile() throw(ReadException);
-        void readBackupFile() throw(BackupException);
-        void match() throw(SocketException);
-        void saveLoginsFile() throw(SaveException);
+    void backup() throw(BackupException);  //备份日志记录
+    void readLoginsFile() throw(ReadException); //读取匹配未成功的记录文件
+    void readBackupFile() throw(BackupException); //读取日志记录的备份文件
+    void match() throw(SocketException);  //匹配登入登出记录
+    void saveLoginsFile() throw(SaveException); //存储将未匹配的登入记录保存
+    std::string getWtmpxString();//logFile转换成backupFile
 };
 
 #endif // LOGREADER_H
