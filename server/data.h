b479be93df0dceaba319c393c1a5d8bad612106e
tanligao <1002064524@qq.com>
2016-06-30 22:55:51 +0800

    add data.h

--- server/data.h
+++ server/data.h
@@ -1,1 +1,18 @@
+//数据对象类型定义
+#ifndef _DATA_H
+#define _DATA_H
+#include<unistd.h>//pid_t等uc数据类型和函数
+
+//匹配日志记录
+struct MLogRec
+{
+   char logname[32];
+   char logip[32];
+   pid_t pid;
+   long logintime;
+   long logouttime;
+   long logtime;
+};
+
+#endif
 
