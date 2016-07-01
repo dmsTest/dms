76461a67561eb6000de3da1b2870d3561261007d
tanligao <1002064524@qq.com>
2016-07-01 11:51:39 +0800

    add server main.cpp

---
+++ server/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "data.h"
+#include "LogDao.h"
+#include "LogThread.h"
+#include "ServerSocket.h"
+#include "Server.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	cout << "hello,server" << endl;
+	return 0;
+}
