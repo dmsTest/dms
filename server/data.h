d0cab641c09201ae5a0f22643dec168014eb5efe
tanligao <1002064524@qq.com>
2016-07-01 16:36:14 +0800

    update data.h add operator!= an ==

--- server/data.h
+++ server/data.h
@@ -2,16 +2,35 @@
 #ifndef _DATA_H
 #define _DATA_H
 #include<unistd.h>//pid_t等uc数据类型和函数
+#include <cstring>
 
 //匹配日志记录
 struct MLogRec
 {
-   char logname[32];
-   char logip[32];
-   pid_t pid;
-   long logintime;
-   long logouttime;
-   long logtime;
+    char logname[32];
+    char logip[32];
+    pid_t pid;
+    long logintime;
+    long logouttime;
+    long logtime;
+
+    bool operator!=(const MLogRec &right)
+    {
+        return !(*this == right);
+    }
+
+    bool operator==(const MLogRec &right)
+    {
+        if( strcmp(logname,right.logname)==0 &&
+                strcmp(logip,right.logip)==0 &&  pid==right.pid && logtime>=0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 };
 
 #endif
