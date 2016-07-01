c3bb70aa6028b88325daca3e6291ff9b913d375b
lcylmh111 <m15691780860@163.com>
2016-07-01 09:19:48 +0800

    Create ServerException.cpp

---
+++ server/ServerException.cpp
@@ -0,0 +1,57 @@
+#include "ServerException.h"
+
+ServerException::ServerException():m_msg("server is exception!")
+
+{
+
+}
+
+ServerException::ServerException(const std::string &str) :m_msg("server is exception!")
+{
+
+    m_msg = m_msg + " " + str + "!";
+
+}
+
+ServerException::~ServerException() throw()
+{
+
+
+}
+
+SocketException::SocketException() : ServerException("Socket error")
+{
+
+
+}
+
+SocketException::SocketException(const std::string &str) : ServerException(str)
+{
+
+
+}
+
+
+DBException::DBException() : ServerException("DB error")
+{
+
+
+}
+
+DBException::DBException(const std::string &str) : ServerException(str)
+{
+
+
+}
+
+ThreadException::ThreadException() : ServerException("Thread error")
+{
+
+
+}
+
+ThreadException::ThreadException(const std::string &str) : ServerException(str)
+{
+
+
+}
