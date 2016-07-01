3472bd8d1ac7e0796d74ba90b7dbbba28aa4cb25
lcylmh111 <m15691780860@163.com>
2016-07-01 09:19:07 +0800

    Create ServerException.h

---
+++ server/ServerException.h
@@ -0,0 +1,48 @@
+#ifndef SERVEREXCEPTION_H
+#define SERVEREXCEPTION_H
+#include <exception>
+#include <string>
+#include <iostream>
+
+
+class ServerException : public std::exception
+
+{
+public:
+    ServerException();
+    ServerException(const std::string &str);
+    virtual ~ServerException() throw();
+
+protected:
+private:
+    std::string m_msg;
+
+};
+
+class SocketException : public ServerException
+{
+public:
+    SocketException();
+    SocketException(const std::string &str);
+
+};
+
+class DBException : public ServerException
+{
+public:
+    DBException();
+    DBException(const std::string &str);
+
+};
+
+
+class ThreadException : public ServerException
+{
+public:
+    ThreadException();
+    ThreadException(const std::string &str);
+
+};
+
+
+#endif // SERVEREXCEPTION_H
