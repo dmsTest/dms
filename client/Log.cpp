8e72fcd3520c02ec95f4c492c9ab87a6733c8f1d
tanligao <1002064524@qq.com>
2016-12-20 14:06:43 +0800

    add log record

---
+++ client/Log.cpp
@@ -0,0 +1,48 @@
+#include "Log.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <stdarg.h>
+
+using namespace std;
+
+Log::Log(string normal_filename,string error_filename):
+	m_normal_filename(normal_filename),
+	m_error_filename(error_filename)
+{
+	m_normal_fd = open(m_normal_filename.c_str(),O_WRONLY | O_CREAT);
+	m_error_fd = open(m_error_filename.c_str(),O_WRONLY | O_CREAT);
+	if( m_normal_fd < 0 || m_error_fd < 0)
+	{
+		cerr << "open or create log file error" << endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+Log::~Log()
+{
+	close(m_normal_fd);
+	close(m_error_fd);
+}
+
+void Log::write_log(E_LOG_TYPE log_type,const char *fmt,...)
+{
+	char buf_line[MAX_LINE];
+	va_list pArgs;
+	va_start(pArgs,fmt);
+	vsnprintf(buf_line,sizeof(buf_line),fmt,pArgs);
+	va_end(pArgs);
+	switch(log_type)
+	{
+	case E_LOG_NORMAL:
+		write(m_normal_fd,buf_line,sizeof(buf_line));
+		break;
+	case E_LOG_ERROR:
+		write(m_error_fd,buf_line,sizeof(buf_line));
+		break;
+	default:
+		break;
+	}
+}
---
+++ client/Log.h
@@ -0,0 +1,33 @@
+#ifndef  __LOG_H_
+#define __LOG_H_
+
+#include <iostream>
+#include "Singleton.h"
+
+#define MAX_LINE   4096
+
+using namespace std;
+
+enum E_LOG_TYPE
+{
+	E_LOG_NORMAL = 0,
+	E_LOG_ERROR,
+	E_LOG_END
+};
+
+// class function:write log to logfile.txt
+// use as: Singleton<Log>::getInstance()->write_log(E_LOG_NORMAL,"%s\n","test log");
+class Log : public Noncopyable
+{
+public:
+	Log(string normal_filename = "log_normal.txt",string error_filename = "log_error.txt");
+	~Log();
+	void write_log(E_LOG_TYPE log_type,const char *fmt,...);
+private:
+	string m_normal_filename;
+	string m_error_filename;
+	int m_normal_fd;
+	int m_error_fd;
+};
+
+#endif
