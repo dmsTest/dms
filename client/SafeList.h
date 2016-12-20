b863b3cb3be959a2fef92dad3a0ebb92975cb644
tanligao <1002064524@qq.com>
2016-12-20 14:07:59 +0800

    add safe queue

---
+++ client/SafeList.h
@@ -0,0 +1,67 @@
+#ifndef  __SAFELIST_H_
+#define __SAFELIST_H_
+
+#include <list>
+#include "Mutex.h"
+#include "Tcp_client.h"
+
+using namespace std;
+
+// thread safe list
+template<typename T>
+class SafeList
+{
+public:
+	SafeList() : m_mutex(),m_list(),m_cond(m_mutex)
+	{
+
+	}
+	~SafeList()
+	{
+
+	}
+	void push_back(T element)
+	{
+		m_mutex.lock();
+		m_list.push_back(element);
+		m_mutex.unlock();
+		m_cond.wake();
+	}
+	bool empty()
+	{
+		m_mutex.lock();
+		bool is_empty = m_list.empty();
+		m_mutex.unlock();
+		return is_empty;
+	}
+	int size()
+	{
+		m_mutex.lock();
+		int size = m_list.size();
+		m_mutex.unlock();
+		return size;
+	}
+	T pop_front()
+	{
+		m_mutex.lock();
+		while( m_list.empty() )
+		{
+			m_cond.wait_for_sec(WAIT_READ_SEC);
+		}
+		if( m_list.empty() )
+		{
+			m_mutex.unlock();
+			return 0;
+		}
+		T element = m_list.front();
+		m_list.pop_front();
+		m_mutex.unlock();
+		return element;
+	}
+private:
+	list<T> m_list;
+	Mutex m_mutex;
+	Conditon m_cond;
+};
+
+#endif
